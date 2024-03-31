30d51670528b96261c6881f73ffe5731ce940ad2
unknown <shiveshtiwari0@gmail.com>
2024-04-01 01:15:25 +0530

    updated dashboard invoice ui

--- src/components/invoiceDashBoard.jsx
+++ src/components/invoiceDashBoard.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Card, CardBody, Divider, Progress, Stat, StatLabel, StatNumber, Text, Box } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Progress, Stat, StatLabel, StatNumber, Text, Box } from '@chakra-ui/react';
 import axios from 'axios';
+import { Divider } from 'antd';
+import { LiaFileInvoiceSolid } from 'react-icons/lia';
 
 const InvoiceDashboardStatsCard = () => {
   const [totalPaidInvoices, setTotalPaidInvoices] = useState(0);
@@ -18,7 +20,7 @@ const InvoiceDashboardStatsCard = () => {
         '/getAverageInvoiceAmount',
         '/getMonthlySalesReport',
       ];
-      const apiBaseUrl = import.meta.env.VITE_API_BASE || ''; 
+      const apiBaseUrl = import.meta.env.VITE_API_BASE || '';
       const requests = endpoints.map((endpoint) =>
         axios.get(`${apiBaseUrl}/api/admin${endpoint}`).catch((error) => console.error(`Error fetching ${endpoint}:`, error))
       );
@@ -40,33 +42,33 @@ const InvoiceDashboardStatsCard = () => {
     fetchData();
   }, []);
 
-  
+
   const totalInvoices = totalPaidInvoices + totalUnpaidInvoices;
   const paidPercentage = totalInvoices ? (totalPaidInvoices / totalInvoices) * 100 : 0;
   const unpaidPercentage = totalInvoices ? (totalUnpaidInvoices / totalInvoices) * 100 : 0;
 
   return (
-    <>
-
-<div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-       
-        <div className="mt-6 text-gray-500">
+    <div className='p-6'>
+      <div className="grid gap-8">
+        <div className="flex gap-3 items-center text-xl text-gray-600">
+          <LiaFileInvoiceSolid fontSize={20} />
+          Invoice overview
+        </div>
+        <div className="text-gray-500 max-w-[400px]">
           <div className="flex justify-between">
             <span>{totalUnpaidInvoices} Unpaid</span>
             <span>{unpaidPercentage.toFixed(2)}%</span>
           </div>
           <Progress value={unpaidPercentage} colorScheme="red" mt={4} height={2} rounded={"lg"} />
-        </div>
-        <div className="mt-6 text-gray-500">
-          <div className="flex justify-between">
+          <div className="flex justify-between mt-6">
             <span>{totalPaidInvoices} Paid</span>
             <span>{paidPercentage.toFixed(2)}%</span>
           </div>
-          <Progress value={paidPercentage} colorScheme="green" mt={4} height={2} rounded={"lg"} />
+          <Progress value={paidPercentage} colorScheme="green" mt={2} height={2} rounded={"lg"} />
         </div>
       </div>
-      <Divider mt={10} mb={4} />
-      <div className="flex gap-6 flex-col md:flex-row">
+      <Divider />
+      <div className="flex gap-6 flex-col md:flex-row items-end">
         <Stat className="rounded-md border-[0.6px] px-4 py-2">
           <StatLabel>Total Lifetime Sales</StatLabel>
           <StatNumber>${totalLifetimeSales.toFixed(2)}</StatNumber>
@@ -75,19 +77,19 @@ const InvoiceDashboardStatsCard = () => {
           <StatLabel>Average Invoice Amount</StatLabel>
           <StatNumber>${averageInvoiceAmount.toFixed(2)}</StatNumber>
         </Stat>
-        <Box mt="6">
-        <Text fontSize="lg" fontWeight="bold" mb="4">Monthly Sales Report</Text>
-        {monthlySalesReport.map(({ _id, totalSales, averageSalePerInvoice, numberOfInvoices }, index) => (
-          <Box key={index} mb="4">
-            <Text fontSize="md" fontWeight="semibold">{`Month: ${_id.month}/${_id.year}`}</Text>
-            <Text fontSize="sm">{`Total Sales: $${totalSales}`}</Text>
-            <Text fontSize="sm">{`Average Sale per Invoice: $${averageSalePerInvoice}`}</Text>
-            <Text fontSize="sm">{`Number of Invoices: ${numberOfInvoices}`}</Text>
-          </Box>
-        ))}
-      </Box>      </div>
-      
-      </>  );
+        <div>
+          <Text color={"gray.500"} fontSize="lg" fontWeight="bold" mb="2">Monthly Sales Report</Text>
+          {monthlySalesReport.map(({ _id, totalSales, averageSalePerInvoice, numberOfInvoices }, index) => (
+            <Box key={index}>
+              <Text fontSize="md" fontWeight="semibold">{`Month: ${_id.month}/${_id.year}`}</Text>
+              <Text fontSize="sm">{`Total Sales: $${totalSales}`}</Text>
+              <Text fontSize="sm">{`Average Sale per Invoice: $${averageSalePerInvoice}`}</Text>
+              <Text fontSize="sm">{`Number of Invoices: ${numberOfInvoices}`}</Text>
+            </Box>
+          ))}
+        </div>
+      </div>
+    </div>);
 };
 
 export default InvoiceDashboardStatsCard;
